c5633cfa225e5dd8895ee884810249e48921bcf9
Lourdes Villarreal Castillo <macluu@MacBook-Pro-de-Lourdes.local>
2019-07-03 20:56:25 +0200

    My first commit

---
+++ main.js
@@ -0,0 +1,65 @@
+let miClase = [
+    {
+        name:"Vasil",
+        age:31,
+        city: "Blagoevgrad"
+
+    },
+    {
+        name:"Lourdes",
+        age:33,
+        city:"Barcelona"
+    },
+    {
+        name:"Daniel",
+        age:36,
+        city:"Munich"
+    },
+    {
+        name:"Romina",
+        age:33,
+        city:"Caracas"
+    },
+    {
+        name:"Marta",
+        age:28,
+        city:"Barcelona"
+    },
+    {
+        name:"Alex",
+        age:29,
+        city:"Sabadell"
+    },
+    {
+        name:"Mar",
+        age:26,
+        city:"Igualada"
+    },
+    {
+        name:"Carol",
+        age:30,
+        city:"El Prat"
+    },
+    {
+        name:"Amaia",
+        age:29,
+        city:"Algorta"
+    },
+    {
+        name:"Nona",
+        age:27,
+        city:"Matadepera"
+    },
+]
+
+function createTable(miGente) {
+    let tbody = document.getElementById("tbody");
+    
+    for (let 1 = 0; miGente < array.length; i++) {
+        let tr = document.createElement("tr");
+        let name = document.createElement("td");
+        let age = document.createElement("td");
+        let city = document.createElement("td");
+  
+}
+}
